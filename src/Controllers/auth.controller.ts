f8ed2fe112bd858fb4530117cc61c0d95c9681cc
chrisszlycoen <uhiriwechrisostom0@gmail.com>
2026-04-06 03:51:50 +0200

    started the Auth Controller

--- src/Controllers/auth.controller.ts
+++ src/Controllers/auth.controller.ts
@@ -2,3 +2,22 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { UserModel } from "../models/User.model";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+export const AuthController ={
+    async register(req:Request, res:Response){
+        try {
+            const {email, password, fullname , phoneMumber} = req.body;
+            if(!email || !password || !fullname || !phoneMumber){
+                return res.status(400).json({message:"All fields are required"});
+            }
+            const exiseingUser = await UserModel.findOne({email});
+            if(exiseingUser){
+                res.status(409).json({message:"User already exists"});
+            }
+        } catch (error) {
+            
+        }
+    }
+}
